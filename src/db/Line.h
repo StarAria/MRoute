2bab4001816a5a42c28d7a70755d8e31459203dd
sanghaoyang <114577694+sanghaoyang@users.noreply.github.com>
2022-11-05 11:43:48 +0800

    Add files via upload

--- src/db/Line.h
+++ src/db/Line.h
@@ -7,7 +7,9 @@
 #define __LINE_H__
 
 #include <vector>
-#include <./Type.h>
+#include <string>
+
+#include "Type.h"
 
 /*! @class Line
     @brief Line net for routing problem.
@@ -15,13 +17,36 @@
 class Line
 {
 private:
-    int                         id;
-    int                         pId;
-    point                       p1;
-    point                       p2;
-    LineType                    type;
+    int                         _id;
+    int                         _pId;
+    point                       _p1;
+    point                       _p2;
+    LineType                    _type;
+
 public:
-   
+
+    /// @brief constructor using pId
+    Line(int pId): _pId(pId) {}
+
+    /// @brief destructor
+    ~Line() {}
+
+    /// these function get specific data members 
+
+    int id() const {return _id;}
+    int pId() const {return _pId;}
+    point p1() const {return _p1;}
+    point p2() const {return _p2;}
+    LineType type() const {return _type;}
+
+    /// these function set specific data members 
+
+    void setId(int id) {_id = id;}
+    void setP1(point p1) {_p1 = p1;}
+    void setP2(point p2) {_p2 = p2;}
+    void setType(LineType type) {_type = type;}
+
+
 };
 
 
