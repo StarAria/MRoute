f4a398043b4e558bc397c06def72fb2d9e08b86e
jeremydzx <84257567+jeremydzx@users.noreply.github.com>
2022-11-08 10:02:14 +0800

    Update Block.h

--- src/db/Block.h
+++ src/db/Block.h
@@ -1,16 +1,3 @@
-/*! @file db/Block.h
-    @brief Block class.
-    @date 2022/11/30
-*/
-
-#ifndef __BLOCK_H__
-#define __BLOCK_H__
-
-#include "Type.h"
-#include "Line.h"
-#include <string>
-#include <vector>
-
 /*! @class Block
     @brief Bloc instance.
 */
@@ -35,7 +22,13 @@ public:
     void setTemplateId(int id) {_templateId=id;}
     void setName(std::string name) {_name=name;}
     void setBias(point bias) {_bias=bias;}
-    void setOri(OriType ori) {_ori=ori;}
+    void setOri(std::string ori) 
+    {
+        if (ori == "R0") _ori = OriType::R0;
+        if (ori == "MX") _ori = OriType::MX;
+        if (ori == "MY") _ori = OriType::MY;
+        if (ori == "R180") _ori = OriType::R180;
+    }
 
     ///@brief set edges of the block one by one
     void setEdges(Line edge) {_edges.push_back(edge);}
@@ -54,60 +47,6 @@ public:
     /*! @brief Judge whether a line intersects the block, and retrun intersection points. 
         If no intersections, the vector size is 0.*/
     std::vector<point> judgeLineIntersect(Line l);
-
-
-
-    ///@brief Judge whether a point is inside a block.If the point is inside the block, return true.
-    bool judgeInside(point p) 
-    {
-        int h=0,v=0;//Number of intersections with horizontal lines and vertical lines
-        for(int i=0;i<_edges.size();i++)
-        {
-            if(_edges[i].isHorizonal())
-            {
-                if(_edges[i].p1().first<p.first&&_edges[i].p2().first>p.first)    //we can assume p1<p2; p1,p2 needs to return reference
-                    h++;
-            }
-            else
-                if(_edges[i].p1().second<p.second&&_edges[i].p2().second>p.second)
-                    v++;
-        }
-        if(h%2==1&&v%2==1)
-                return true;
-            else
-                return false;
-    }
-
-    /*! @brief Judge whether a line intersects the block, and retrun intersection points. If no intersections, the vector size is 0.*/
-    std::vector<point> judgeLineIntersect(Line l)
-    {
-        std::vector<point> intersections;
-        bool horizon=l.isHorizonal();
-        for(int i=0;i<_edges.size();i++)
-        {
-            if(horizon)
-            {
-                if(!_edges[i].isHorizonal())
-                    if(_edges[i].p1().second<l.p1().second&&_edges[i].p2().second>l.p1().second&&_edges[i].p1().first<l.p2().first&&_edges[i].p1().first>l.p1().first)
-                    {
-                        point a(_edges[i].p1().first,l.p1().second);
-                        intersections.push_back(a);
-                    }
-                        
-            }
-            else
-            {
-                if(_edges[i].isHorizonal())
-                    if(_edges[i].p1().first<l.p1().first&&_edges[i].p2().first>l.p1().second&&_edges[i].p1().second<l.p2().second&&_edges[i].p1().first>l.p1().second)
-                    {
-                        point a(l.p1().first,_edges[i].p1().second);
-                        intersections.push_back(a);
-                    }
-            }
-        }
-        return intersections; 
-    }
-
 
 };
 
